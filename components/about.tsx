dd6a562c5f624d63dda1d9e8a1c550a3a5576925
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-12-26 22:16:52 +0530

    chore: about content updated

--- components/about.tsx
+++ components/about.tsx
@@ -18,29 +18,29 @@ const About = () => {
       <Sectionheading heading={`About me`} />
       <p className="mb-3">
         After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
+        <span className="font-medium">Computer Application(MCA)</span>, I
+        decided to pursue my passion for programming. I learned{" "}
         <span className="font-medium">full-stack web development</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
         is{" "}
         <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
+          React, Next.js, Typescript, and Framer-Motion.
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
+        I am always looking to learn new technologies. I am currently looking
+        for a <span className="font-medium">full-time position</span> as a
+        software developer.
       </p>
 
       <p>
         <span className="italic">When I&apos;m not coding</span>, I enjoy
-        playing video games, watching movies, and playing with my dog. I also
-        enjoy <span className="font-medium">learning new things</span>. I am
-        currently learning about{" "}
+        watching movies, and playing with my dog.
+        {/* I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about{" "}
         <span className="font-medium">history and philosophy</span>. I&apos;m
-        also learning how to play the guitar.
+        also learning how to play the guitar. */}
       </p>
     </motion.section>
   );
